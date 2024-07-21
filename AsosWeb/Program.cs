c8dca9fd61a74ad72c5b662d371069f66d3326dc
Bogdan Slonets <bogdan.slonets@gmail.com>
2024-07-21 17:52:18 +0300

    git

--- AsosWeb/Program.cs
+++ AsosWeb/Program.cs
@@ -64,7 +64,7 @@ builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
 {
     //options.Stores.MaxLengthForKeys = 128;
     options.Password.RequireDigit = false;
-    //options.Password.RequiredLength = 5;
+    options.Password.RequiredLength = 5;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireUppercase = false;
     options.Password.RequireLowercase = false;
