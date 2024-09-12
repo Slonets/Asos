f58893f8f4c478fbf2606299f40e64382c519ea0
Bogdan Slonets <bogdan.slonets@gmail.com>
2024-09-12 16:11:50 +0300

    New class

---
+++ Infrastructure/Entities/Site/BasketEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities.Site
+{
+    public class BasketEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int? UserId { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public ProductEntity Product { get; set; }        
+        public int Count { get; set; }      
+        public DateTime DateAdded { get; set; }
+    }
+}
